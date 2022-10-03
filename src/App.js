e4bbcf9d4f77dc577652a7b4d1c1aac650a5eb97
martuwilson <williner@ibm.com>
2022-10-03 11:38:19 -0300

    front done, backend needs to start

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
+import { useState } from 'react';
 import './App.css';
+import {Users} from './users';
+import Table from './components/Table';
 
 function App() {
+
+  const [query, setQuery] = useState("");
+
+  const keys = [
+    "first_name",
+    "last_name",
+    "email"
+  ] // keys to search in the data-array
+
+  console.log(Users[0])
+
+  const search = (data) => {
+    return data.filter(
+      (item) =>
+       keys.some((key) => item[key].toLowerCase().includes(query))
+    );
+  }
+
   return (
     <div className="app">
-      <input type="text" placeholder="Search..." className="search" />
-      <ul className="list">
-        <li className="listItem">Martin</li>
-        <li className="listItem">Ezequiel</li>
-        <li className="listItem">Gerardo</li>
-
-      </ul>
+      <input
+      type="text"
+      placeholder="Search..."
+      className="search"
+      onChange={e => setQuery(e.target.value)}/>
+      
+      <Table data={search(Users)}/>
+      
     </div>
   );
 }
